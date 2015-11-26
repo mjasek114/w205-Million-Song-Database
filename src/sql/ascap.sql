dfb74a7a800425db03a53ab870e826e7e5c2c53f
Sean Underwood <flyinghamsterster@gmail.com>
2015-11-26 16:38:01 -0600

    Added ASCAP DML query

---
+++ src/sql/ascap.sql
@@ -0,0 +1,29 @@
+CREATE EXTERNAL TABLE ascap_staging (
+	Title STRING,
+	RoleType STRING,
+	Name STRING,
+	Shares INT,
+	Note STRING
+	)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LOCATION 's3://w205-mmm/ascap'
+tblproperties ("skip.header.line.count"="1");
+
+CREATE TABLE ascap (
+	Title STRING,
+	RoleType STRING,
+	Name STRING,
+	Shares INT,
+	Note STRING
+	)
+STORED AS ORC;
+
+INSERT INTO ascap (Title, RoleType, Name, Shares, Note)
+SELECT 
+	Title, 
+	RoleType,
+	regexp_replace(Name, '(\\w+)\\s(.+)', '$2 $1'),
+	Shares,
+	Note
+FROM ascap_staging;
