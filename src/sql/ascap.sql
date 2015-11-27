958333fa3e9cb250da09cabd28f5a4915b3f5490
Sean Underwood <seanbunderwood@gmail.com>
2015-11-27 22:10:53 +0000

    Forgot to remove some trailing commas from field lists in ascap.sql

--- src/sql/ascap.sql
+++ src/sql/ascap.sql
@@ -12,13 +12,13 @@ tblproperties ("skip.header.line.count"="1");
 
 CREATE TABLE ascap_writers (
 	Title STRING,
-	Name STRING,
+	Name STRING
 	)
 STORED AS ORC;
 
 INSERT OVERWRITE TABLE ascap_writers
 SELECT 
 	Title, 
-	regexp_replace(Name, '(\\w+)\\s(.+)', '$2 $1'),
+	regexp_replace(Name, '(\\w+)\\s(.+)', '$2 $1')
 FROM ascap
 WHERE RoleType = 'W';
