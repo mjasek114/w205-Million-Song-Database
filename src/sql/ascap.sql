20b88976104fa3506d167406115f3319d4ce61f0
Sean Underwood <seanbunderwood@gmail.com>
2015-11-27 22:08:55 +0000

    Fixing ascap queries

--- src/sql/ascap.sql
+++ src/sql/ascap.sql
@@ -13,18 +13,12 @@ tblproperties ("skip.header.line.count"="1");
 CREATE TABLE ascap_writers (
 	Title STRING,
 	Name STRING,
-	Shares INT,
-	Note STRING
 	)
 STORED AS ORC;
 
-INSERT INTO ascap_writers (
-	Title, RoleType, Name, Shares, Note
-	)
+INSERT OVERWRITE TABLE ascap_writers
 SELECT 
 	Title, 
 	regexp_replace(Name, '(\\w+)\\s(.+)', '$2 $1'),
-	Shares,
-	Note
 FROM ascap
 WHERE RoleType = 'W';
