e3205b6c8577aaa5af3b79d88ec7ed1b537a04c9
Sean Underwood <flyinghamsterster@gmail.com>
2015-12-01 18:06:00 -0600

    Added song.sql, moved hive scripts to their own dir

---
+++ src/sql/song.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Song (
+	artist VARCHAR(256),
+	title VARCHAR(1024),
+	writer VARCHAR(256),
+	year INT,
+	peakPosition INT,
+	billboardYear INT,
+	artistHotttness FLOAT,
+	songHotttness FLOAT,
+	danceability FLOAT,
+	key VARCHAR(10),
+	tempo FLOAT,
+	duration FLOAT,
+	loudness FLOAT,
+	timeSignature INT
+)
