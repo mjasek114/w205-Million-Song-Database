f2e0a3f1bd74e348972a8ff8c5c06e0dba87ca9e
Sean Underwood <flyinghamsterster@gmail.com>
2015-12-06 17:30:40 -0600

    Added keys_for_artist report. May not work yet.

--- shiny/helloworld/app.R
+++ shiny/helloworld/app.R
@@ -2,11 +2,17 @@
 library(shiny)
 
 ui <- fluidPage(
-   "hello world"
+   "Hello,world!",
+   sliderInput(inputId="num", 
+               label="Choose a number",
+               value=25, min=1, max=100),
+   textOutput("val")
 )
 
 server <- function(input, output) {
-
+  output$val = renderText({
+    text <- paste("Your number is: ", input$num)
+    })
 }
 
 shinyApp(ui = ui, server = server)
---
+++ shiny/keys_for_artist/app.R
@@ -0,0 +1,29 @@
+
+library(shiny)
+library(DBI)
+library(RMySQL)
+library(ggplot2)
+
+drv = dbDriver("MySQL")
+db = dbConnect(drv, dbname="metamusic", host="localhost")
+
+ui <- fluidPage(
+   "Keys for artist",
+   textInput(inputId = "artist",
+   			label="Artist Name:",
+   			value="Vanilla Ice"),
+   plotOutput("plot")
+)
+
+server <- function(input, output) {
+	data <- reactive({
+		query <- paste("SELECT songKey FROM song WHERE artist = '", input$artist, "'")
+		rs <- dbSendQuery(db, query)
+		fetch(rs, n=-1)
+		})
+	output$plot <- renderPlot(
+		ggplot(data(), aes(songKey)) + geom_histogram(aes(y=..count..))
+		)
+}
+
+shinyApp(ui = ui, server = server)
