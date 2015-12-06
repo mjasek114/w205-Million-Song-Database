32843dc5bdac7591a2243c82486341d49dc36e84
Sean Underwood <flyinghamsterster@gmail.com>
2015-12-06 12:40:00 -0600

    Still working on load data script

--- src/sql/loaddata.sql
+++ src/sql/loaddata.sql
@@ -5,4 +5,24 @@ INTO TABLE Song
 FIELDS 
   TERMINATED BY ','
   OPTIONALLY ENCLOSED BY '"'
-  ESCAPED BY '\\';
+  ESCAPED BY '\\'
+(@artist, @title, @writer, @songYear, @peakPosition, 
+ @billboardYear, @artistHotttness, @songHotttness,
+ @danceability, @songKey, @tempo, @duration, @loudness,
+ @timeSignature)
+SET
+  artist = nullif(@artist, ''),
+  title = nullif(@title, ''),
+  writer = nullif(@writer, ''),
+  songYear = nullif(@songYear, ''),
+  peakPosition = nullif(@peakPosition, ''),
+  billboardYear = nullif(@billboardYear, ''),
+  artistHotttness = nullif(@artistHotttness, ''),
+  songHotttness = nullif(@songHotttness, ''),
+  danceability = nullif(@danceability, ''),
+  songKey = nullif(@songKey, ''),
+  tempo = nullif(@tempo, ''),
+  duration = nullif(@duration, ''),
+  loudness = nullif(@loudness, ''),
+  timeSignature = nullif(@timeSignature, '')
+;
