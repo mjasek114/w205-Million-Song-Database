595186b4a951c5f16d9112cc098402ac5c99d924
root <root@ip-172-31-63-104.ec2.internal>
2015-12-07 00:47:15 +0000

    Fixed query, got graph

--- shiny/keys_for_artist/app.R
+++ shiny/keys_for_artist/app.R
@@ -12,16 +12,25 @@ ui <- fluidPage(
    textInput(inputId = "artist",
    			label="Artist Name:",
    			value="Vanilla Ice"),
+   textOutput("artist"),
+   textOutput("query"),
    plotOutput("plot")
 )
 
 server <- function(input, output) {
-	output$plot <- renderPlot(
-		query <- paste("SELECT songKey FROM song WHERE artist = '", input$artist, "'")
+        output$query <- renderText({
+                paste("SELECT songKey FROM song WHERE artist = '", input$artist, "'", sep="")
+	})
+        output$plot <- renderPlot({
+		query <- paste("SELECT songKey FROM song WHERE artist = '", input$artist, "'", sep="")
 		rs <- dbSendQuery(db, query)
 		df <- fetch(rs, n=-1)
-		ggplot(data=df, aes(songKey)) + geom_histogram(aes(y=..count..))
-		)
+                if(nrow(df) == 0) return()
+		p <- ggplot(data=df, aes(songKey), environment=environment()) 
+                p <- p + geom_histogram(aes(y=..count..))
+		print(p)
+                })
+        output$artist = renderText({input$artist})
 }
 
 shinyApp(ui = ui, server = server)
