12fb4976a44e921bffce8057502c151824e360ed
Sean Underwood <flyinghamsterster@gmail.com>
2015-12-12 16:24:07 -0600

    Updating sql scritps in light of data problems

--- src/sql/createtable.sql
+++ src/sql/createtable.sql
@@ -3,6 +3,21 @@ USE metamusic
 CREATE TABLE song (
 	artist VARCHAR(256),
 	title VARCHAR(500),
+	songYear INT,
+	peakPosition INT,
+	billboardYear INT,
+	artistHotttness FLOAT,
+	songHotttness FLOAT,
+	songKey VARCHAR(10),
+	tempo FLOAT,
+	duration FLOAT,
+	loudness FLOAT,
+	timeSignature INT,
+	mode VARCHAR(10)
+);
+
+CREATE TABLE song_writer (
+	title VARCHAR(500),
 	writer VARCHAR(256),
 	songYear INT,
 	peakPosition INT,
--- src/sql/loaddata.sql
+++ src/sql/loaddata.sql
@@ -6,14 +6,13 @@ FIELDS
   TERMINATED BY ','
   OPTIONALLY ENCLOSED BY '"'
   ESCAPED BY '\\'
-(@artist, @title, @writer, @songYear, @peakPosition, 
+(@artist, @title, @songYear, @peakPosition, 
  @billboardYear, @artistHotttness, @songHotttness,
  @songKey, @tempo, @duration, @loudness,
  @timeSignature, @mode)
 SET
   artist = nullif(@artist, ''),
   title = nullif(@title, ''),
-  writer = nullif(@writer, ''),
   songYear = nullif(@songYear, ''),
   peakPosition = nullif(nullif(@peakPosition, ''), '0'),
   billboardYear = nullif(@billboardYear, ''),
@@ -29,3 +28,34 @@ SET
 
 UPDATE song SET artistHotttness = NULL WHERE artistHotttness = 0;
 UPDATE song SET songHotttness = NULL WHERE songHotttness = 0;
+
+
+
+LOAD DATA LOCAL INFILE '/tmp/Writer.csv'
+INTO TABLE song_writer
+FIELDS 
+  TERMINATED BY ','
+  OPTIONALLY ENCLOSED BY '"'
+  ESCAPED BY '\\'
+( @title, @writer, @songYear, @peakPosition, 
+ @billboardYear, @artistHotttness, @songHotttness,
+ @songKey, @tempo, @duration, @loudness,
+ @timeSignature, @mode)
+SET
+  title = nullif(@title, ''),
+  writer = nullif(@writer, ''),
+  songYear = nullif(@songYear, ''),
+  peakPosition = nullif(nullif(@peakPosition, ''), '0'),
+  billboardYear = nullif(@billboardYear, ''),
+  artistHotttness = nullif(@artistHotttness, ''),
+  songHotttness = nullif(@songHotttness, ''),
+  songKey = nullif(@songKey, ''),
+  tempo = nullif(@tempo, ''),
+  duration = nullif(@duration, ''),
+  loudness = nullif(@loudness, ''),
+  timeSignature = nullif(@timeSignature, ''),
+  mode = nullif(@mode, '')
+;
+
+UPDATE song_writer SET artistHotttness = NULL WHERE artistHotttness = 0;
+UPDATE song_writer SET songHotttness = NULL WHERE songHotttness = 0;
